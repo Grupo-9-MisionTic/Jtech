92533827726226e9d2bb2061a7d96f9e93c9489e
Juan Zuluaga <juandanielzuluagaserna@gmail.com>
2021-12-03 12:31:22 -0500

    Project Structure

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+"use strict";
+require('./src/models/database');
+const express = require('express');
+const app = express();
+const port = 3005;
+const routerApi = require('./src/routes/index');
+
+app.use(express.json());
+
+const server = app.listen(port, () => {
+  let host = server.address().address;
+  console.log(`Server is running on ${host} ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World Express');
+});
+
+routerApi(app);
+
+
+
+
+
+
+
